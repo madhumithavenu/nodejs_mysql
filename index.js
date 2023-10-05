319e7ab3c5f7a638abb7a0982cbbe2ac4e1c16f3
Madhumitha <madhumithavenu03@gmail.com>
2023-10-05 16:26:52 +0530

    Creating connection

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+require("dotenv").config(); // ALLOWS ENVIRONMENT VARIABLES TO BE SET ON PROCESS.ENV SHOULD BE AT TOP
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+const express = require("express");
+const app = express();
+
+// Middleware
+app.use(express.json()); // parse json bodies in the request object
+
+// Redirect requests to endpoint starting with /posts to postRoutes.js
+app.use("/posts", require("./routes/postRoutes"));
+
+// Global Error Handler. IMPORTANT function params MUST start with err
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  console.log(err.name);
+  console.log(err.code);
+
+  res.status(500).json({
+    message: "Something went rely wrong",
+  });
 });
 
-module.exports = pool.promise();
+// Listen on pc port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
